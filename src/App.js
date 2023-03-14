c1f6840c5b8c570f080ee5f06eaa72ab02bf78e2
Arshiya <gh.arshiya92@gmail.com>
2023-03-14 15:16:12 +0330

    Change State

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React from 'react'
 import Product from './components/Products/Products'
 
 class App extends React.Component {
+
+    state = {
+        Products : [
+            {name : 'Chai', price: 2500},
+            {name : 'Coffee', price: 3000},
+            {name : 'OJ', price: 1500},
+        ],
+    }
+
+    changePriceHandler = () => {
+        this.setState({
+            Products : [
+                {name : 'Chai', price: 3000},
+                {name : 'Coffee', price: 3500},
+                {name : 'OJ', price: 2000},
+            ],
+        })
+    }
+
     render(){
         return(
             <div id='main' className='container'>
                 <h2>React App</h2>
                 <p>Hello Wrold !</p>
 
-                <Product name="Chai" price="2500" />
-                <Product name="Coffee" price="3000">
-                    Discount : 20%
-                </Product>
-                <Product name="OJ" price="1500" />
+                <Product 
+                    name={this.state.Products[0].name}
+                    price={this.state.Products[0].price}
+                />
+                
+                <Product 
+                    name={this.state.Products[1].name}
+                    price={this.state.Products[1].price} 
+                />
+                
+                <Product
+                    name={this.state.Products[2].name}
+                    price={this.state.Products[2].price}
+                />
+                
+
+                <button onClick={this.changePriceHandler}>Change Price</button>
             </div>
         )
     }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Product = (props) => {
+
     return (
         <div style={{textAlign:'center'}}>
             <h3>Product Name : {props.name}</h3>
