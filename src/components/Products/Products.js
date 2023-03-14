214ff62c6342a7056bc4ec536211be6f905d0080
Arshiya <gh.arshiya92@gmail.com>
2023-03-14 15:01:17 +0330

    Dynamic Data

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Product = () => {
+
+    const price = 2500;
+
     return (
         <div style={{textAlign:'center'}}>
             <h3>Product Name : Chai</h3>
             <p>Chai Is Good</p>
-            <p>Product Price : 2500</p>
+            <p>Product Price : {price}</p>
         </div>
     )
 }
