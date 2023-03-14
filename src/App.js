8ce43110aebde7533fecb763a3d1e68a8194724d
Arshiya <gh.arshiya92@gmail.com>
2023-03-14 14:55:10 +0330

    Using Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Product from './components/Products/Products'
+
 
 const App = () => {
     return(
         <div id='main' className='container'>
             <h2>React App</h2>
             <p>Hello Wrold !</p>
+
+            <Product />
+
         </div>
     )
 }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div style={{textAlign:'center'}}>
+            <h3>Product Name : Chai</h3>
+            <p>Chai Is Good</p>
+            <p>Product Price : 2500</p>
+        </div>
+    )
+}
+
+export default Product
